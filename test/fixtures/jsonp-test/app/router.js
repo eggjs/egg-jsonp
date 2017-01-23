5069a20433136f9b44467e3bfd4dd80987958676
Yiyu He <dead-horse@users.noreply.github.com>
2017-01-23 14:02:09 +0800

    fix: refine jsonp (#1)
    jsonp 接口在如下情况可以访问
    
    - 如果 csrf 和 白名单都没配置
    - csrf 或者 referrer 白名单命中通过任意一个

--- test/fixtures/jsonp-test/app/router.js
+++ test/fixtures/jsonp-test/app/router.js
@@ -1,12 +1,13 @@
 'use strict';
 
 module.exports = app => {
-  app.get('/default', app.enableJsonp(), 'jsonp.index');
-  app.get('/empty', app.enableJsonp(), 'jsonp.empty');
+  app.get('/default', app.jsonp(), 'jsonp.index');
+  app.get('/empty', app.jsonp(), 'jsonp.empty');
   app.get('/disable', 'jsonp.index');
-  app.get('/fn', app.enableJsonp({ callback: 'fn' }), 'jsonp.index');
-  app.get('/referrer/subdomain', app.enableJsonp({ whiteList: '.test.com' }), 'jsonp.index');
-  app.get('/referrer/equal', app.enableJsonp({ whiteList: 'test.com' }), 'jsonp.index');
-  app.get('/referrer/regexp', app.enableJsonp({ whiteList: [/https?:\/\/test\.com\//, /https?:\/\/foo\.com\//] }), 'jsonp.index');
-  app.get('/csrf', app.enableJsonp({ csrf: true }), 'jsonp.index');
+  app.get('/fn', app.jsonp({ callback: 'fn' }), 'jsonp.index');
+  app.get('/referrer/subdomain', app.jsonp({ whiteList: '.test.com' }), 'jsonp.index');
+  app.get('/referrer/equal', app.jsonp({ whiteList: 'test.com' }), 'jsonp.index');
+  app.get('/referrer/regexp', app.jsonp({ whiteList: [/https?:\/\/test\.com\//, /https?:\/\/foo\.com\//] }), 'jsonp.index');
+  app.get('/csrf', app.jsonp({ csrf: true }), 'jsonp.index');
+  app.get('/both', app.jsonp({ csrf: true, whiteList: 'test.com' }), 'jsonp.index');
 };
